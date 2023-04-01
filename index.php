0a45a805df32113d927341a24a5b7cf11e716057
utkirkurbanov <utkirkurbanov2700@gmail.com>
2023-04-01 13:41:48 +0500

    'PHP Programming Recipes'- with David Sklar, Adam Trachtenberg

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// "PHP Рецепты программирования" - Дэдив Скляр, Адам Трахтенберг 
+
+$bookName = "\"PHP Programming Recipes\" book";
+$authors = "David Sklar, Adam Trachtenberg";
+
+echo "I am just starting to learn new " .$bookName . " with authors - " . $authors . "." . "<br />";
+
+echo "Knowledge is power!!!";
+
